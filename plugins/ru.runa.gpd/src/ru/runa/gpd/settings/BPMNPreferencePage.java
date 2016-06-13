adc816a0821e21937bef97ecadaf8c78768e1857
SDNj <sdnj@ya.ru>
2016-06-13 12:01:39 +0300

    сделано: содержимое настройки Общие -> BPMN перенести в Языки -> BPMN (можно заодно перенести выбор языка по умолчанию в Языки); взаимодействие с сервером убрать.

--- plugins/ru.runa.gpd/src/ru/runa/gpd/settings/BPMNPreferencePage.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/settings/BPMNPreferencePage.java
@@ -1,14 +1,22 @@
 package ru.runa.gpd.settings;
 
-import org.eclipse.jface.preference.BooleanFieldEditor;
+import java.awt.Font;
+
+import org.eclipse.jface.preference.ColorFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.FontFieldEditor;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
 import ru.runa.gpd.Activator;
 import ru.runa.gpd.Localization;
 
-public class BPMNPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+public class BPMNPreferencePage extends FieldEditorPreferencePage implements PrefConstants, IWorkbenchPreferencePage {
+
+    private static final String PREF_COMMON_BPMN = "pref.common.bpmn.";
 
     public BPMNPreferencePage() {
         super(GRID);
@@ -21,8 +29,22 @@ public class BPMNPreferencePage extends FieldEditorPreferencePage implements IWo
 
     @Override
     public void createFieldEditors() {
-        addField(new BooleanFieldEditor(PrefConstants.P_BPMN_SHOW_SWIMLANE, Localization.getString("pref.notation.bpmn.showSwimlane"),
-                getFieldEditorParent()));
+        final IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+        store.setDefault(P_BPMN_FONT, new FontData("Verdana", 9, Font.PLAIN).toString());
+        addField(new FontFieldEditor(P_BPMN_FONT, Localization.getString(PREF_COMMON_BPMN + P_BPMN_FONT), getFieldEditorParent()));
+
+        addColorField(store, P_BPMN_COLOR_FONT);
+        addColorField(store, P_BPMN_COLOR_BACKGROUND);
+        addColorField(store, P_BPMN_COLOR_BASE);
+        addColorField(store, P_BPMN_COLOR_TRANSITION);
     }
 
+    private void addColorField(IPreferenceStore store, String name) {
+        addField(new ColorFieldEditor(name, Localization.getString(PREF_COMMON_BPMN + name), getFieldEditorParent()));
+    }
+
+    @Override
+    protected void contributeButtons(final Composite buttonBar) {
+        applyDialogFont(buttonBar);
+    }
 }
