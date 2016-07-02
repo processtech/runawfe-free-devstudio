9a3057299c1d76380ec13828ddb30b58e4dc8173
Daniel Khodyrev <khodyrevds@gmail.com>
2016-07-02 12:25:02 +0700

    #1029 DevStudio, боты с одинаковыми именами в разных бот-станциях, проблемы с экспортом
    Переименование бота

--- plugins/ru.runa.gpd/src/ru/runa/gpd/ui/dialog/RenameBotDialog.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/ui/dialog/RenameBotDialog.java
@@ -1,82 +1,60 @@
 package ru.runa.gpd.ui.dialog;
 
 import org.eclipse.core.resources.IFolder;
-import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.swt.SWT;
+import org.eclipse.jface.dialogs.IInputValidator;
+import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
 
+import ru.runa.gpd.BotCache;
 import ru.runa.gpd.Localization;
+import com.google.common.base.Strings;
 
-public class RenameBotDialog extends Dialog {
-    private String name;
-    private final IFolder botFolder;
+public class RenameBotDialog extends InputDialog {
 
-    public RenameBotDialog(IFolder botFolder) {
-        super(Display.getDefault().getActiveShell());
-        this.botFolder = botFolder;
-    }
+    public RenameBotDialog(final IFolder botFolder) {
+        super(Display.getDefault().getActiveShell(), Localization.getString("RenameBotDialog.update.title"), Localization.getString("property.name")
+                + ":", botFolder.getName(), new IInputValidator() {
 
-    public void setName(String name) {
-        this.name = name;
+            @Override
+            public String isValid(String value) {
+                if (!botFolder.getName().equals(value) && BotCache.getAllBotNames().contains(value)) {
+                    return Localization.getString("RenameBotDialog.error.botWithSameNameExists", value);
+                }
+
+                return null;
+            }
+        });
+        setBlockOnOpen(true);
     }
 
     @Override
     protected Control createDialogArea(Composite parent) {
-        Composite area = (Composite) super.createDialogArea(parent);
-        GridLayout layout = new GridLayout(1, false);
-        area.setLayout(layout);
-        final Composite composite = new Composite(area, SWT.NONE);
-        final GridLayout gridLayout = new GridLayout();
-        gridLayout.numColumns = 2;
-        composite.setLayout(gridLayout);
-        GridData nameData = new GridData();
-        composite.setLayoutData(nameData);
-        Label labelName = new Label(composite, SWT.NONE);
-        labelName.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_END));
-        labelName.setText(Localization.getString("property.name") + ":");
-        final Text nameField = new Text(composite, SWT.BORDER);
-        GridData nameTextData = new GridData(GridData.FILL_HORIZONTAL);
-        nameTextData.minimumWidth = 200;
-        nameField.setText(name);
-        nameField.setLayoutData(nameTextData);
-        nameField.addModifyListener(new ModifyListener() {
+        Control control = super.createDialogArea(parent);
+        getText().addModifyListener(new ModifyListener() {
+
             @Override
             public void modifyText(ModifyEvent e) {
-                name = nameField.getText();
                 updateButtons();
+
             }
         });
-        return area;
-    }
-
-    @Override
-    protected void createButtonsForButtonBar(Composite parent) {
-        super.createButtonsForButtonBar(parent);
-        getButton(IDialogConstants.OK_ID).setEnabled(false);
+        return control;
     }
 
     private void updateButtons() {
-        boolean allowCreation = !((IFolder) botFolder.getParent()).getFolder(name).exists();
-        getButton(IDialogConstants.OK_ID).setEnabled(allowCreation);
+        getButton(IDialogConstants.OK_ID).setEnabled(
+                !(Strings.isNullOrEmpty(getText().getText()) || BotCache.getAllBotNames().contains(getText().getText())));
     }
 
     @Override
     protected void configureShell(Shell newShell) {
         super.configureShell(newShell);
         newShell.setText(Localization.getString("RenameBotDialog.update.title"));
-    }
-
-    public String getName() {
-        return name;
     }
 }
