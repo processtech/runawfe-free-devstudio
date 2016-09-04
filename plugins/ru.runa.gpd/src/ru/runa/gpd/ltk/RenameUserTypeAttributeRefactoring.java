aa9df557a733398edcfd579e711e51748ddfe2a1
Igor Yurchenko <beggi_84@mail.ru>
2016-09-04 16:29:48 +0300

    improve rename attribute; show warnings in wizard even if no changes found for refactoring

--- plugins/ru.runa.gpd/src/ru/runa/gpd/ltk/RenameUserTypeAttributeRefactoring.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/ltk/RenameUserTypeAttributeRefactoring.java
@@ -9,6 +9,8 @@ import org.eclipse.ltk.core.refactoring.CompositeChange;
 import org.eclipse.ltk.core.refactoring.Refactoring;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 
+import ru.runa.gpd.Localization;
+import ru.runa.gpd.PluginLogger;
 import ru.runa.gpd.lang.model.ProcessDefinition;
 import ru.runa.gpd.lang.model.Variable;
 import ru.runa.gpd.lang.model.VariableUserType;
@@ -24,7 +26,8 @@ public class RenameUserTypeAttributeRefactoring extends Refactoring {
     private final Variable oldAttribute;
     private final String newAttributeName;
     private final String newAttributeScriptingName;
-    private final List<Refactoring> refactorings = Lists.newArrayList();
+    private final List<RenameVariableRefactoring> refactorings = Lists.newArrayList();
+    private final RefactoringStatus finalStatus;
 
     public RenameUserTypeAttributeRefactoring(IFile definitionFile, ProcessDefinition processDefinition, VariableUserType type,
             Variable oldAttribute, String newAttributeName, String newAttributeScriptingName) {
@@ -34,42 +37,58 @@ public class RenameUserTypeAttributeRefactoring extends Refactoring {
         this.oldAttribute = oldAttribute;
         this.newAttributeName = newAttributeName;
         this.newAttributeScriptingName = newAttributeScriptingName;
+        this.finalStatus = new RefactoringStatus();
     }
 
     @Override
     public RefactoringStatus checkInitialConditions(IProgressMonitor progressMonitor) {
         RefactoringStatus status = new RefactoringStatus();
-        List<Variable> result = VariableUtils.findVariablesOfTypeWithAttributeExpanded(processDefinition, type, oldAttribute);
-        for (Variable variable : result) {
-            String newName = variable.getName().replace(VariableUserType.DELIM + oldAttribute.getName(), VariableUserType.DELIM + newAttributeName);
-            String newScriptingName = variable.getScriptingName().replace(VariableUserType.DELIM + oldAttribute.getScriptingName(),
-                    VariableUserType.DELIM + newAttributeScriptingName);
-            RenameVariableRefactoring refactoring = new RenameVariableRefactoring(definitionFile, processDefinition, variable, newName,
-                    newScriptingName);
-            status.merge(refactoring.checkInitialConditions(progressMonitor));
-            refactorings.add(refactoring);
+        try {
+            List<Variable> result = VariableUtils.findVariablesOfTypeWithAttributeExpanded(processDefinition, type, oldAttribute);
+            for (Variable variable : result) {
+                String newName = variable.getName().replace(VariableUserType.DELIM + oldAttribute.getName(),
+                        VariableUserType.DELIM + newAttributeName);
+                String newScriptingName = variable.getScriptingName().replace(VariableUserType.DELIM + oldAttribute.getScriptingName(),
+                        VariableUserType.DELIM + newAttributeScriptingName);
+                RenameVariableRefactoring refactoring = new RenameVariableRefactoring(definitionFile, processDefinition, variable, newName,
+                        newScriptingName);
+                status.merge(refactoring.checkInitialConditions(progressMonitor));
+                refactorings.add(refactoring);
+            }
+        } catch (Exception e) {
+            PluginLogger.logErrorWithoutDialog(e.getMessage(), e);
+            status.addFatalError(Localization.getString("UnhandledException") + ": " + e.getLocalizedMessage());
         }
         return status;
     }
 
     @Override
     public RefactoringStatus checkFinalConditions(IProgressMonitor pm) {
-        return new RefactoringStatus();
+        return finalStatus;
     }
 
     @Override
     public CompositeChange createChange(IProgressMonitor progressMonitor) throws CoreException {
         CompositeChange compositeChange = new CompositeChange(getName());
-        for (Refactoring refactoring : refactorings) {
-            compositeChange.merge((CompositeChange) refactoring.createChange(progressMonitor));
+        for (RenameVariableRefactoring refactoring : refactorings) {
+            compositeChange.merge(refactoring.createChange(progressMonitor));
+            finalStatus.merge(refactoring.checkFinalConditions(null));
         }
         return compositeChange;
     }
 
     public boolean isUserInteractionNeeded() {
         try {
-            checkInitialConditions(null);
-            return createChange(null).getChildren().length > 0;
+            RefactoringStatus initialStatus = checkInitialConditions(null);
+            if (initialStatus.hasFatalError()) {
+                finalStatus.merge(initialStatus);
+                return true;
+            }
+            CompositeChange change = createChange(null);
+            if (finalStatus.hasWarning() || finalStatus.hasError() || finalStatus.hasFatalError()) {
+                return true;
+            }
+            return change.getChildren().length > 0;
         } catch (CoreException e) {
             throw Throwables.propagate(e);
         }
