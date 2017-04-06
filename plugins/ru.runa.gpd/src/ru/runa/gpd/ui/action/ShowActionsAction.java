179895889c9901703828aa753afc0ceceebfc481
dataobject <dataobject@yandex.ru>
2017-04-06 18:00:18 +0300

    feature-194: bug fixing.

--- plugins/ru.runa.gpd/src/ru/runa/gpd/ui/action/ShowActionsAction.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/ui/action/ShowActionsAction.java
@@ -2,15 +2,31 @@ package ru.runa.gpd.ui.action;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
 
+import ru.runa.gpd.Activator;
 import ru.runa.gpd.editor.ProcessEditorBase;
+import ru.runa.gpd.editor.graphiti.GraphitiProcessEditor;
 import ru.runa.gpd.lang.model.SubprocessDefinition;
 
 public class ShowActionsAction extends BaseActionDelegate {
+
     @Override
     public void run(IAction action) {
         ProcessEditorBase editor = getActiveDesignerEditor();
         editor.getDefinition().setShowActions(!editor.getDefinition().isShowActions());
+        IEditorReference[] editorRefs = Activator.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage().getEditorReferences();
+        for (IEditorReference er : editorRefs) {
+            IEditorPart ep = er.getEditor(true);
+            if (ep instanceof GraphitiProcessEditor) {
+                GraphitiProcessEditor gpe = (GraphitiProcessEditor) ep;
+                if (gpe.getDefinition() instanceof SubprocessDefinition) {
+                    gpe.getDiagramEditorPage().refreshActions();
+                    gpe.getDiagramEditorPage().refreshPalette();
+                }
+            }
+        }
     }
 
     @Override
@@ -19,4 +35,5 @@ public class ShowActionsAction extends BaseActionDelegate {
         action.setChecked(editor != null && editor.getDefinition().isShowActions());
         action.setEnabled(editor != null && !(editor.getDefinition() instanceof SubprocessDefinition));
     }
+
 }
