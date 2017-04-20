fd2c42ac0f4a2abc2930c7733febb103855d4b2c
dataobject <dataobject@yandex.ru>
2017-04-20 14:23:40 +0300

    bug-305: fix multitask bugs.

--- plugins/ru.runa.gpd/src/ru/runa/gpd/editor/gef/part/graph/TaskStateGraphicalEditPart.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/editor/gef/part/graph/TaskStateGraphicalEditPart.java
@@ -37,6 +37,7 @@ public class TaskStateGraphicalEditPart extends SwimlaneNodeEditPart implements 
         list.add(PROPERTY_MINIMAZED_VIEW);
         list.add(PROPERTY_CHILDS_CHANGED);
         list.add(PROPERTY_ASYNC);
+        list.add(PROPERTY_DISCRIMINATOR_USAGE);
     }
 
     @Override
