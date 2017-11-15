flyninja/runawfe-devstudio

Feature #167 DS. Custom colors and fonts in BPMN Add feature of changing fonts, color of fonts, colors of border, color of background

plugins/ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/add/AddStateNodeFeature.java
@@ -25,6 +25,7 @@ public class AddStateNodeFeature extends AddNodeFeature {
     @Override
     public PictogramElement add(IAddContext context) {
         Node node = (Node) context.getNewObject();
+        String bpmnName = node.getTypeDefinition().getBpmnElementName();
         //
         ContainerShape containerShape = Graphiti.getPeCreateService().createContainerShape(context.getTargetContainer(), true);
         Rectangle main = Graphiti.getGaService().createInvisibleRectangle(containerShape);
@@ -34,17 +35,17 @@ public class AddStateNodeFeature extends AddNodeFeature {
         RoundedRectangle border = Graphiti.getGaService().createRoundedRectangle(main, 20, 20);
         border.getProperties().add(new GaProperty(GaProperty.ID, LayoutStateNodeFeature.BORDER_RECT));
         border.setLineWidth(2);
-        border.setForeground(Graphiti.getGaService().manageColor(getDiagram(), StyleUtil.LIGHT_BLUE));
-        border.setBackground(Graphiti.getGaService().manageColor(getDiagram(), StyleUtil.VERY_LIGHT_BLUE));
-        border.setStyle(StyleUtil.getStyleForEvent(getDiagram()));
+        border.setStyle(StyleUtil.getStyleForEvent(getDiagram(), bpmnName));
         if (node instanceof SwimlanedNode && node.getProcessDefinition().getSwimlaneDisplayMode() == SwimlaneDisplayMode.none) {
-            Text swimlaneText = Graphiti.getGaService().createDefaultText(getDiagram(), border, ((SwimlanedNode) node).getSwimlaneLabel());
+        	Text swimlaneText = Graphiti.getGaService().createText(border, ((SwimlanedNode) node).getSwimlaneLabel());
             swimlaneText.getProperties().add(new GaProperty(GaProperty.ID, GaProperty.SWIMLANE_NAME));
             swimlaneText.setHorizontalAlignment(Orientation.ALIGNMENT_CENTER);
+            swimlaneText.setStyle(StyleUtil.getStyleForText(getDiagram(),bpmnName));
         }
-        MultiText nameText = Graphiti.getGaService().createDefaultMultiText(getDiagram(), border, node.getName());
+        MultiText nameText = Graphiti.getGaService().createMultiText(border, node.getName());
         nameText.getProperties().add(new GaProperty(GaProperty.ID, GaProperty.NAME));
         nameText.setHorizontalAlignment(Orientation.ALIGNMENT_CENTER);
+        nameText.setStyle(StyleUtil.getStyleForText(getDiagram(),bpmnName));
         containerShape.getProperties().add(new GaProperty(GaProperty.MINIMAZED_VIEW, String.valueOf(node.isMinimizedView())));
         //
         Image image = Graphiti.getGaService().createImage(main, NodeRegistry.getNodeTypeDefinition(node.getClass()).getPaletteIcon());

plugins/ru.runa.gpd/src/ru/runa/gpd/settings/BPMNPreferencePage.java
@@ -1,15 +1,26 @@
 package ru.runa.gpd.settings;
 
-import org.eclipse.jface.preference.BooleanFieldEditor;
+import java.awt.Font;
+
+import org.eclipse.jface.preference.ColorFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.PlatformUI;
 
 import ru.runa.gpd.Activator;
 import ru.runa.gpd.Localization;
+import ru.runa.gpd.editor.graphiti.GraphitiProcessEditor;
 
-public class BPMNPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
-
+public class BPMNPreferencePage extends FieldEditorPreferencePage implements PrefConstants, IWorkbenchPreferencePage {
+	private static final String PREF_COMMON_BPMN = "pref.common.bpmn.";
+	
     public BPMNPreferencePage() {
         super(GRID);
         setPreferenceStore(Activator.getDefault().getPreferenceStore());
@@ -21,8 +32,42 @@ public class BPMNPreferencePage extends FieldEditorPreferencePage implements IWo
 
     @Override
     public void createFieldEditors() {
-        addField(new BooleanFieldEditor(PrefConstants.P_BPMN_SHOW_SWIMLANE, Localization.getString("pref.notation.bpmn.showSwimlane"),
-                getFieldEditorParent()));
+    	final IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+    	store.setDefault(P_BPMN_FONT, new FontData("Arial", 8, Font.PLAIN).toString());
+    	addColorField(store, P_BPMN_COLOR_TRANSITION);
     }
-
+    
+    private void addColorField(IPreferenceStore store, String name) {
+    	addField(new ColorFieldEditor(name, Localization.getString(PREF_COMMON_BPMN + name), getFieldEditorParent()));
+    }
+    
+    @Override
+    protected void contributeButtons(final Composite buttonBar) {
+    	applyDialogFont(buttonBar);
+    	}
+    
+    @Override
+    public boolean performOk() {
+    	boolean performOk = super.performOk();
+    	if(performOk){
+    		applyStyles();
+    	}
+    	return performOk;
+    }
+    
+    @Override
+    protected void performApply() {
+    	super.performApply();
+    	applyStyles();
+    }
+    
+	private void applyStyles() {
+		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		for (IEditorReference ref : page.getEditorReferences()) {
+			IEditorPart editor = ref.getEditor(true);
+			if (editor instanceof GraphitiProcessEditor) {
+				((GraphitiProcessEditor) editor).getDiagramEditorPage().applyStyles();
+			}
+		}
+	}
 }

plugins/ru.runa.gpd/src/ru/runa/gpd/settings/LanguageElementPreferencePage.java
@@ -1,16 +1,26 @@
 package ru.runa.gpd.settings;
 
+import java.awt.Font;
+
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.ColorFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.FontFieldEditor;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.IntegerFieldEditor;
 import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
 
 import ru.runa.gpd.Activator;
 import ru.runa.gpd.Localization;
 import ru.runa.gpd.editor.gef.GefEntry;
 import ru.runa.gpd.editor.graphiti.GraphitiEntry;
+import ru.runa.gpd.editor.graphiti.GraphitiProcessEditor;
 import ru.runa.gpd.lang.Language;
 import ru.runa.gpd.lang.NodeTypeDefinition;
 import ru.runa.gpd.lang.model.GraphElement;
@@ -22,6 +32,7 @@ public class LanguageElementPreferencePage extends FieldEditorPreferencePage imp
     private final String id;
     private final NodeTypeDefinition definition;
     private final Language language;
+    private static final String PREF_COMMON_BPMN = "pref.common.bpmn.";
 
     public LanguageElementPreferencePage(String id, NodeTypeDefinition definition, Language language) {
         super(definition.getLabel(), GRID);
@@ -57,48 +68,157 @@ public class LanguageElementPreferencePage extends FieldEditorPreferencePage imp
         }
     }
 
-    @Override
-    public void createFieldEditors() {
-        GraphElement element;
-        try {
-            element = definition.getModelClass().newInstance();
-        } catch (Exception e) {
-            return;
-        }
-        init(element);
+	@Override
+	public void createFieldEditors() {
+		final IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+		store.setDefault(P_BPMN_FONT, new FontData("Arial", 8, Font.PLAIN).toString());
+		GraphElement element;
+		try {
+			element = definition.getModelClass().newInstance();
+		} catch (Exception e) {
+			return;
+		}
+		init(element);
+		if (element instanceof NamedGraphElement) {
+			addField(new StringFieldEditor(
+					getKey(P_LANGUAGE_NODE_NAME_PATTERN),
+					Localization.getString("pref.language.node.name.pattern"),
+					getFieldEditorParent()));
+		}
+		if (NodeTypeDefinition.TYPE_NODE.equals(definition.getType())) {
+			IntegerFieldEditor widthEditor = new IntegerFieldEditor(
+					getKey(P_LANGUAGE_NODE_WIDTH),
+					Localization.getString("pref.language.node.width"),
+					getFieldEditorParent());
+			IntegerFieldEditor heightEditor = new IntegerFieldEditor(
+					getKey(P_LANGUAGE_NODE_HEIGHT),
+					Localization.getString("pref.language.node.height"),
+					getFieldEditorParent());
+			boolean fixedSize = language == Language.JPDL ? (definition
+					.getGefEntry() != null && definition.getGefEntry()
+					.isFixedSize()) : language == Language.BPMN ? (definition
+					.getGraphitiEntry() != null && definition
+					.getGraphitiEntry().isFixedSize()) : true;
+			widthEditor.setEnabled(!fixedSize, getFieldEditorParent());
+			heightEditor.setEnabled(!fixedSize, getFieldEditorParent());
+			addField(widthEditor);
+			addField(heightEditor);
+			if (language == Language.BPMN){
+				switch (definition.getBpmnElementName()) {
+				case "scriptTask":
+					addField(new FontFieldEditor(P_BPMN_SCRIPTTASK_FONT,
+							Localization.getString(PREF_COMMON_BPMN + P_BPMN_SCRIPTTASK_FONT),
+							getFieldEditorParent()));
+					addColorField(store, P_BPMN_SCRIPTTASK_FONT_COLOR);
+					addColorField(store, P_BPMN_SCRIPTTASK_BACKGROUND_COLOR);
+					addColorField(store, P_BPMN_SCRIPTTASK_BASE_COLOR);
+					break;
+				case "userTask":
+					addField(new FontFieldEditor(P_BPMN_STATE_FONT,
+							Localization.getString(PREF_COMMON_BPMN + P_BPMN_STATE_FONT), getFieldEditorParent()));
+					addColorField(store, P_BPMN_STATE_FONT_COLOR);
+					addColorField(store, P_BPMN_STATE_BACKGROUND_COLOR);
+					addColorField(store, P_BPMN_STATE_BASE_COLOR);
+					break;
+				case "endTokenEvent":
+					addField(new FontFieldEditor(P_BPMN_ENDTOKEN_FONT,
+							Localization.getString(PREF_COMMON_BPMN + P_BPMN_ENDTOKEN_FONT), getFieldEditorParent()));
+					addColorField(store, P_BPMN_ENDTOKEN_FONT_COLOR);
+					break;
+				case "endEvent":
+					addField(new FontFieldEditor(P_BPMN_END_FONT,
+							Localization.getString(PREF_COMMON_BPMN + P_BPMN_END_FONT), getFieldEditorParent()));
+					addColorField(store, P_BPMN_END_FONT_COLOR);
+					break;
+				case "startEvent":
+					addField(new FontFieldEditor(P_BPMN_STARTSTATE_FONT,
+							Localization.getString(PREF_COMMON_BPMN + P_BPMN_STARTSTATE_FONT),
+							getFieldEditorParent()));
+					addColorField(store, P_BPMN_STARTSTATE_FONT_COLOR);
+					break;
+				case "multiTask":
+					addField(new FontFieldEditor(P_BPMN_MULTITASKSTATE_FONT,
+							Localization.getString(PREF_COMMON_BPMN + P_BPMN_MULTITASKSTATE_FONT),
+							getFieldEditorParent()));
+					addColorField(store, P_BPMN_MULTITASKSTATE_FONT_COLOR);
+					addColorField(store, P_BPMN_MULTITASKSTATE_BACKGROUND_COLOR);
+					addColorField(store, P_BPMN_MULTITASKSTATE_BASE_COLOR);
+					break;
+				case "multiProcess":
+					addField(new FontFieldEditor(P_BPMN_MULTISUBPROCESS_FONT,
+							Localization.getString(PREF_COMMON_BPMN + P_BPMN_MULTISUBPROCESS_FONT),
+							getFieldEditorParent()));
+					addColorField(store, P_BPMN_MULTISUBPROCESS_FONT_COLOR);
+					addColorField(store, P_BPMN_MULTISUBPROCESS_BACKGROUND_COLOR);
+					addColorField(store, P_BPMN_MULTISUBPROCESS_BASE_COLOR);
+					break;
+				case "subProcess":
+					addField(new FontFieldEditor(P_BPMN_SUBPROCESS_FONT,
+							Localization.getString(PREF_COMMON_BPMN + P_BPMN_SUBPROCESS_FONT),
+							getFieldEditorParent()));
+					addColorField(store, P_BPMN_SUBPROCESS_FONT_COLOR);
+					addColorField(store, P_BPMN_SUBPROCESS_BACKGROUND_COLOR);
+					addColorField(store, P_BPMN_SUBPROCESS_BASE_COLOR);
+					break;
+				}
+			}
+		}
+		if (element instanceof TaskState) {
+			addField(new BooleanFieldEditor(
+					getKey(P_LANGUAGE_SWIMLANE_INITIALIZER),
+					Localization.getString("Swimlane.reassignSwimlaneToInitializerValue"),
+					getFieldEditorParent()));
+			addField(new BooleanFieldEditor(
+					getKey(P_LANGUAGE_SWIMLANE_PERFORMER),
+					Localization.getString("Swimlane.reassignSwimlaneToTaskPerformer"),
+					getFieldEditorParent()));
+			addField(new BooleanFieldEditor(
+					getKey(P_LANGUAGE_TASK_STATE_ASYNC_INPUT_DATA),
+					Localization.getString("TaskNode.inputDataAllowedInAsyncMode"),
+					getFieldEditorParent()));
+			switch (definition.getBpmnElementName()) {
 
-        if (element instanceof NamedGraphElement) {
-            addField(new StringFieldEditor(getKey(P_LANGUAGE_NODE_NAME_PATTERN), Localization.getString("pref.language.node.name.pattern"),
-                    getFieldEditorParent()));
-        }
-        if (NodeTypeDefinition.TYPE_NODE.equals(definition.getType())) {
-            IntegerFieldEditor widthEditor = new IntegerFieldEditor(getKey(P_LANGUAGE_NODE_WIDTH),
-                    Localization.getString("pref.language.node.width"), getFieldEditorParent());
-            IntegerFieldEditor heightEditor = new IntegerFieldEditor(getKey(P_LANGUAGE_NODE_HEIGHT),
-                    Localization.getString("pref.language.node.height"), getFieldEditorParent());
-            boolean fixedSize = language == Language.JPDL ? (definition.getGefEntry() != null && definition.getGefEntry().isFixedSize())
-                    : language == Language.BPMN ? (definition.getGraphitiEntry() != null && definition.getGraphitiEntry().isFixedSize()) : true;
-            widthEditor.setEnabled(!fixedSize, getFieldEditorParent());
-            heightEditor.setEnabled(!fixedSize, getFieldEditorParent());
-            addField(widthEditor);
-            addField(heightEditor);
-        }
-        if (element instanceof TaskState) {
-            addField(new BooleanFieldEditor(getKey(P_LANGUAGE_SWIMLANE_INITIALIZER),
-                    Localization.getString("Swimlane.reassignSwimlaneToInitializerValue"), getFieldEditorParent()));
-            addField(new BooleanFieldEditor(getKey(P_LANGUAGE_SWIMLANE_PERFORMER),
-                    Localization.getString("Swimlane.reassignSwimlaneToTaskPerformer"), getFieldEditorParent()));
-            addField(new BooleanFieldEditor(getKey(P_LANGUAGE_TASK_STATE_ASYNC_INPUT_DATA),
-                    Localization.getString("TaskNode.inputDataAllowedInAsyncMode"), getFieldEditorParent()));
-        }
+			}
+		}
 
-        if (element instanceof Subprocess){
-            addField(new BooleanFieldEditor(getKey(P_LANGUAGE_SUB_PROCESS_ASYNC_INPUT_DATA),
-                    Localization.getString("Subprocess.inputDataAllowedInAsyncSubprocess"), getFieldEditorParent()));
-        }
+		if (element instanceof Subprocess) {
+			addField(new BooleanFieldEditor(
+					getKey(P_LANGUAGE_SUB_PROCESS_ASYNC_INPUT_DATA),
+					Localization.getString("Subprocess.inputDataAllowedInAsyncSubprocess"),
+					getFieldEditorParent()));
+		}
+	}
+
+    private void addColorField(IPreferenceStore store, String name) {
+    	addField(new ColorFieldEditor(name, Localization.getString(PREF_COMMON_BPMN + name), getFieldEditorParent()));
     }
 
     private String getKey(String property) {
         return id + '.' + property;
+    }
+    
+    @Override
+    public boolean performOk() {
+    	boolean performOk = super.performOk();
+    	if(performOk){
+    		applyStyles();
+    	}
+    	return performOk;
+    }
+    
+    @Override
+    protected void performApply() {
+    	super.performApply();
+    	applyStyles();
+    }
+    
+	private void applyStyles() {
+		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		for (IEditorReference ref : page.getEditorReferences()) {
+			IEditorPart editor = ref.getEditor(true);
+			if (editor instanceof GraphitiProcessEditor) {
+				((GraphitiProcessEditor) editor).getDiagramEditorPage().applyStyles();
+			}
+		}
     }
 }

processtech/runawfe-free-devstudio

plugins/ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/add/AddStateNodeFeature.java
@@ -25,6 +25,7 @@ public class AddStateNodeFeature extends AddNodeFeature {
     @Override
     public PictogramElement add(IAddContext context) {
         Node node = (Node) context.getNewObject();
+        String bpmnName = node.getTypeDefinition().getBpmnElementName();
         //
         ContainerShape containerShape = Graphiti.getPeCreateService().createContainerShape(context.getTargetContainer(), true);
         Rectangle main = Graphiti.getGaService().createInvisibleRectangle(containerShape);
@@ -34,17 +35,18 @@ public class AddStateNodeFeature extends AddNodeFeature {
         RoundedRectangle border = Graphiti.getGaService().createRoundedRectangle(main, 20, 20);
         border.getProperties().add(new GaProperty(GaProperty.ID, LayoutStateNodeFeature.BORDER_RECT));
         border.setLineWidth(2);
-        border.setForeground(Graphiti.getGaService().manageColor(getDiagram(), StyleUtil.LIGHT_BLUE));
-        border.setBackground(Graphiti.getGaService().manageColor(getDiagram(), StyleUtil.VERY_LIGHT_BLUE));
-        border.setStyle(StyleUtil.getStyleForEvent(getDiagram()));
+        border.setStyle(StyleUtil.getStyleForEvent(getDiagram(), bpmnName));
         if (node instanceof SwimlanedNode && node.getProcessDefinition().getSwimlaneDisplayMode() == SwimlaneDisplayMode.none) {
-            Text swimlaneText = Graphiti.getGaService().createDefaultText(getDiagram(), border, ((SwimlanedNode) node).getSwimlaneLabel());
+            Text swimlaneText = Graphiti.getGaService().createText(border, ((SwimlanedNode) node).getSwimlaneLabel());
             swimlaneText.getProperties().add(new GaProperty(GaProperty.ID, GaProperty.SWIMLANE_NAME));
             swimlaneText.setHorizontalAlignment(Orientation.ALIGNMENT_CENTER);
+            swimlaneText.setStyle(StyleUtil.getStyleForText(getDiagram(), bpmnName));
         }
-        MultiText nameText = Graphiti.getGaService().createDefaultMultiText(getDiagram(), border, node.getName());
+        MultiText nameText = Graphiti.getGaService().createMultiText(border, node.getName());
         nameText.getProperties().add(new GaProperty(GaProperty.ID, GaProperty.NAME));
         nameText.setHorizontalAlignment(Orientation.ALIGNMENT_CENTER);
+        nameText.setStyle(StyleUtil.getStyleForText(getDiagram(), bpmnName));
+
         containerShape.getProperties().add(new GaProperty(GaProperty.MINIMAZED_VIEW, String.valueOf(node.isMinimizedView())));
         //
         Image image = Graphiti.getGaService().createImage(main, NodeRegistry.getNodeTypeDefinition(node.getClass()).getPaletteIcon());

plugins/ru.runa.gpd/src/ru/runa/gpd/settings/BPMNPreferencePage.java
@@ -1,14 +1,25 @@
 package ru.runa.gpd.settings;
 
-import org.eclipse.jface.preference.BooleanFieldEditor;
+import java.awt.Font;
+
+import org.eclipse.jface.preference.ColorFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.PlatformUI;
 
 import ru.runa.gpd.Activator;
 import ru.runa.gpd.Localization;
+import ru.runa.gpd.editor.graphiti.GraphitiProcessEditor;
 
-public class BPMNPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+public class BPMNPreferencePage extends FieldEditorPreferencePage implements PrefConstants, IWorkbenchPreferencePage {
+    private static final String PREF_COMMON_BPMN = "pref.common.bpmn.";
 
     public BPMNPreferencePage() {
         super(GRID);
@@ -21,8 +32,42 @@ public class BPMNPreferencePage extends FieldEditorPreferencePage implements IWo
 
     @Override
     public void createFieldEditors() {
-        addField(new BooleanFieldEditor(PrefConstants.P_BPMN_SHOW_SWIMLANE, Localization.getString("pref.notation.bpmn.showSwimlane"),
-                getFieldEditorParent()));
+        final IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+        store.setDefault(P_BPMN_FONT, new FontData("Arial", 8, Font.PLAIN).toString());
+        addColorField(store, P_BPMN_TRANSITION_COLOR);
     }
 
+    private void addColorField(IPreferenceStore store, String name) {
+        addField(new ColorFieldEditor(name, Localization.getString(PREF_COMMON_BPMN + name), getFieldEditorParent()));
+    }
+
+    @Override
+    protected void contributeButtons(final Composite buttonBar) {
+        applyDialogFont(buttonBar);
+    }
+
+    @Override
+    public boolean performOk() {
+        boolean performOk = super.performOk();
+        if (performOk) {
+            applyStyles();
+        }
+        return performOk;
+    }
+
+    @Override
+    protected void performApply() {
+        super.performApply();
+        applyStyles();
+    }
+
+    private void applyStyles() {
+        IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+        for (IEditorReference ref : page.getEditorReferences()) {
+            IEditorPart editor = ref.getEditor(true);
+            if (editor instanceof GraphitiProcessEditor) {
+                ((GraphitiProcessEditor) editor).getDiagramEditorPage().applyStyles();
+            }
+        }
+    }
 }

plugins/ru.runa.gpd/src/ru/runa/gpd/settings/LanguageElementPreferencePage.java
@@ -2,26 +2,34 @@ package ru.runa.gpd.settings;
 
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.ColorFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.FontFieldEditor;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.IntegerFieldEditor;
 import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
 
 import ru.runa.gpd.Activator;
 import ru.runa.gpd.Localization;
 import ru.runa.gpd.editor.gef.GefEntry;
 import ru.runa.gpd.editor.graphiti.GraphitiEntry;
+import ru.runa.gpd.editor.graphiti.GraphitiProcessEditor;
 import ru.runa.gpd.lang.Language;
 import ru.runa.gpd.lang.NodeTypeDefinition;
 import ru.runa.gpd.lang.model.GraphElement;
 import ru.runa.gpd.lang.model.NamedGraphElement;
-import ru.runa.gpd.lang.model.TaskState;
 import ru.runa.gpd.lang.model.Subprocess;
+import ru.runa.gpd.lang.model.TaskState;
 
 public class LanguageElementPreferencePage extends FieldEditorPreferencePage implements PrefConstants {
     private final String id;
     private final NodeTypeDefinition definition;
     private final Language language;
+    private static final String PREF_COMMON_BPMN = "pref.common.bpmn.";
 
     public LanguageElementPreferencePage(String id, NodeTypeDefinition definition, Language language) {
         super(definition.getLabel(), GRID);
@@ -66,7 +74,6 @@ public class LanguageElementPreferencePage extends FieldEditorPreferencePage imp
             return;
         }
         init(element);
-
         if (element instanceof NamedGraphElement) {
             addField(new StringFieldEditor(getKey(P_LANGUAGE_NODE_NAME_PATTERN), Localization.getString("pref.language.node.name.pattern"),
                     getFieldEditorParent()));
@@ -82,6 +89,61 @@ public class LanguageElementPreferencePage extends FieldEditorPreferencePage imp
             heightEditor.setEnabled(!fixedSize, getFieldEditorParent());
             addField(widthEditor);
             addField(heightEditor);
+            if (language == Language.BPMN) {
+                switch (definition.getBpmnElementName()) {
+                case "scriptTask":
+                    addField(new FontFieldEditor(P_BPMN_SCRIPTTASK_FONT, Localization.getString(PREF_COMMON_BPMN + P_BPMN_FONT),
+                            getFieldEditorParent()));
+                    addColorField(P_BPMN_SCRIPTTASK_FONT_COLOR, P_BPMN_FONT_COLOR);
+                    addColorField(P_BPMN_SCRIPTTASK_BACKGROUND_COLOR, P_BPMN_BACKGROUND_COLOR);
+                    addColorField(P_BPMN_SCRIPTTASK_BASE_COLOR, P_BPMN_BASE_COLOR);
+                    break;
+                case "userTask":
+                    addField(new FontFieldEditor(P_BPMN_STATE_FONT, Localization.getString(PREF_COMMON_BPMN + P_BPMN_FONT), getFieldEditorParent()));
+                    addColorField(P_BPMN_STATE_FONT_COLOR, P_BPMN_FONT_COLOR);
+                    addColorField(P_BPMN_STATE_BACKGROUND_COLOR, P_BPMN_BACKGROUND_COLOR);
+                    addColorField(P_BPMN_STATE_BASE_COLOR, P_BPMN_BASE_COLOR);
+                    break;
+                case "endTokenEvent":
+                    /*
+                     * addField(new FontFieldEditor(P_BPMN_ENDTOKEN_FONT, Localization.getString(PREF_COMMON_BPMN + P_BPMN_FONT),
+                     * getFieldEditorParent())); addColorField(P_BPMN_ENDTOKEN_FONT_COLOR, P_BPMN_FONT_COLOR);
+                     */
+                    addField(new FontFieldEditor(P_BPMN_END_FONT, Localization.getString(PREF_COMMON_BPMN + P_BPMN_FONT), getFieldEditorParent()));
+                    addColorField(P_BPMN_END_FONT_COLOR, P_BPMN_FONT_COLOR);
+                    break;
+                case "endEvent":
+                    addField(new FontFieldEditor(P_BPMN_END_FONT, Localization.getString(PREF_COMMON_BPMN + P_BPMN_FONT), getFieldEditorParent()));
+                    addColorField(P_BPMN_END_FONT_COLOR, P_BPMN_FONT_COLOR);
+                    break;
+                case "startEvent":
+                    addField(new FontFieldEditor(P_BPMN_STARTSTATE_FONT, Localization.getString(PREF_COMMON_BPMN + P_BPMN_FONT),
+                            getFieldEditorParent()));
+                    addColorField(P_BPMN_STARTSTATE_FONT_COLOR, P_BPMN_FONT_COLOR);
+                    break;
+                case "multiTask":
+                    addField(new FontFieldEditor(P_BPMN_MULTITASKSTATE_FONT, Localization.getString(PREF_COMMON_BPMN + P_BPMN_FONT),
+                            getFieldEditorParent()));
+                    addColorField(P_BPMN_MULTITASKSTATE_FONT_COLOR, P_BPMN_FONT_COLOR);
+                    addColorField(P_BPMN_MULTITASKSTATE_BACKGROUND_COLOR, P_BPMN_BACKGROUND_COLOR);
+                    addColorField(P_BPMN_MULTITASKSTATE_BASE_COLOR, P_BPMN_BASE_COLOR);
+                    break;
+                case "multiProcess":
+                    addField(new FontFieldEditor(P_BPMN_MULTISUBPROCESS_FONT, Localization.getString(PREF_COMMON_BPMN + P_BPMN_FONT),
+                            getFieldEditorParent()));
+                    addColorField(P_BPMN_MULTISUBPROCESS_FONT_COLOR, P_BPMN_FONT_COLOR);
+                    addColorField(P_BPMN_MULTISUBPROCESS_BACKGROUND_COLOR, P_BPMN_BACKGROUND_COLOR);
+                    addColorField(P_BPMN_MULTISUBPROCESS_BASE_COLOR, P_BPMN_BASE_COLOR);
+                    break;
+                case "subProcess":
+                    addField(new FontFieldEditor(P_BPMN_SUBPROCESS_FONT, Localization.getString(PREF_COMMON_BPMN + P_BPMN_FONT),
+                            getFieldEditorParent()));
+                    addColorField(P_BPMN_SUBPROCESS_FONT_COLOR, P_BPMN_FONT_COLOR);
+                    addColorField(P_BPMN_SUBPROCESS_BACKGROUND_COLOR, P_BPMN_BACKGROUND_COLOR);
+                    addColorField(P_BPMN_SUBPROCESS_BASE_COLOR, P_BPMN_BASE_COLOR);
+                    break;
+                }
+            }
         }
         if (element instanceof TaskState) {
             addField(new BooleanFieldEditor(getKey(P_LANGUAGE_SWIMLANE_INITIALIZER),
@@ -90,15 +152,48 @@ public class LanguageElementPreferencePage extends FieldEditorPreferencePage imp
                     Localization.getString("Swimlane.reassignSwimlaneToTaskPerformer"), getFieldEditorParent()));
             addField(new BooleanFieldEditor(getKey(P_LANGUAGE_TASK_STATE_ASYNC_INPUT_DATA),
                     Localization.getString("TaskNode.inputDataAllowedInAsyncMode"), getFieldEditorParent()));
+            switch (definition.getBpmnElementName()) {
+
+            }
         }
 
-        if (element instanceof Subprocess){
+        if (element instanceof Subprocess) {
             addField(new BooleanFieldEditor(getKey(P_LANGUAGE_SUB_PROCESS_ASYNC_INPUT_DATA),
                     Localization.getString("Subprocess.inputDataAllowedInAsyncSubprocess"), getFieldEditorParent()));
         }
     }
 
+    private void addColorField(String name, String localName) {
+        ColorFieldEditor colorFieldEditor = new ColorFieldEditor(name, Localization.getString(PREF_COMMON_BPMN + localName), getFieldEditorParent());
+        addField(colorFieldEditor);
+    }
+
     private String getKey(String property) {
         return id + '.' + property;
+    }
+
+    @Override
+    public boolean performOk() {
+        boolean performOk = super.performOk();
+        if (performOk) {
+            applyStyles();
+        }
+        return performOk;
+    }
+
+    @Override
+    protected void performApply() {
+        super.performApply();
+        applyStyles();
+    }
+
+    private void applyStyles() {
+        IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+        for (IEditorReference ref : page.getEditorReferences()) {
+            IEditorPart editor = ref.getEditor(true);
+            if (editor instanceof GraphitiProcessEditor) {
+                ((GraphitiProcessEditor) editor).getDiagramEditorPage().applyStyles();
+            }
+        }
     }
 }