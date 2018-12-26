f5ba60a2d2714f945e0af5c1339d66616b5a7c95
Dofs <dofs197@gmail.com>
2018-12-27 00:57:16 +0300

    pr141 code review

--- plugins/ru.runa.gpd/src/ru/runa/gpd/editor/SelectAllAction.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/editor/SelectAllAction.java
@@ -1,31 +1,27 @@
 package ru.runa.gpd.editor;
 
+import com.google.common.collect.Lists;
 import java.util.List;
-
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.GraphicalEditPart;
 import org.eclipse.gef.GraphicalViewer;
-import org.eclipse.gef.Request;
-import org.eclipse.gef.RequestConstants;
 import org.eclipse.gef.internal.GEFMessages;
+import org.eclipse.graphiti.ui.internal.parts.ConnectionDecoratorEditPart;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.actions.ActionFactory;
 
-import com.google.common.collect.Lists;
-
 /**
- * An action which selects all edit parts in the active workbench part.
- * Code copied from org.eclipse.gef.ui.actions.SelectAllAction; transition included
+ * An action which selects all edit parts in the active workbench part. Code copied from org.eclipse.gef.ui.actions.SelectAllAction; transition
+ * included
  */
 @SuppressWarnings("restriction")
 public class SelectAllAction extends Action {
     private IWorkbenchPart part;
 
     /**
-     * Constructs a <code>SelectAllAction</code> and associates it with the
-     * given part.
+     * Constructs a <code>SelectAllAction</code> and associates it with the given part.
      * 
      * @param part
      *            The workbench part associated with this SelectAllAction
@@ -64,7 +60,10 @@ public class SelectAllAction extends Action {
 
     private void addEditPartChildren(EditPart parent, List<EditPart> selection) {
         for (EditPart childPart : (List<EditPart>) parent.getChildren()) {
-            if (childPart.isSelectable() && childPart.getTargetEditPart(new Request(RequestConstants.REQ_SELECTION)) == childPart ) {
+            if (childPart instanceof ConnectionDecoratorEditPart) {
+                continue;
+            }
+            if (childPart.isSelectable()) {
                 selection.add(childPart);
             }
             if (childPart instanceof GraphicalEditPart) {
@@ -77,4 +76,5 @@ public class SelectAllAction extends Action {
             addEditPartChildren(childPart, selection);
         }
     }
+
 }
