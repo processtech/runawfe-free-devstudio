252e71c1342a7a4a93ac750bdce64d062d58f53b
dataobject <dataobject@yandex.ru>
2019-09-05 17:57:45 +0300

    RM1146

--- plugins/ru.runa.gpd/src/ru/runa/gpd/lang/ValidationError.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/lang/ValidationError.java
@@ -1,7 +1,6 @@
 package ru.runa.gpd.lang;
 
 import org.eclipse.core.resources.IMarker;
-
 import ru.runa.gpd.Localization;
 import ru.runa.gpd.lang.model.GraphElement;
 
@@ -9,15 +8,25 @@ public class ValidationError {
     private final String message;
     private final GraphElement source;
     private final int severity;
+    private final ValidationErrorDetails details;
     
     private ValidationError(GraphElement source, String message, int severity) {
+        this(source, message, severity, null);
+    }
+
+    private ValidationError(GraphElement source, String message, int severity, ValidationErrorDetails details) {
         this.message = message;
         this.source = source;
         this.severity = severity;
+        this.details = details;
     }
     
     public static ValidationError createError(GraphElement source, String message) {
-        return new ValidationError(source, message, IMarker.SEVERITY_ERROR);
+        return createError(source, message, null);
+    }
+
+    public static ValidationError createError(GraphElement source, String message, ValidationErrorDetails details) {
+        return new ValidationError(source, message, IMarker.SEVERITY_ERROR, details);
     }
 
     public static ValidationError createWarning(GraphElement source, String message) {
@@ -49,4 +58,9 @@ public class ValidationError {
     public GraphElement getSource() {
         return source;
     }
+
+    public ValidationErrorDetails getDetails() {
+        return details;
+    }
+
 }
