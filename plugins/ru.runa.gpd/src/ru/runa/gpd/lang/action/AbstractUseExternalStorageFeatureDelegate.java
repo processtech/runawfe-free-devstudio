e74c4b162dab75ef86ba8435fc91d8325cc1878d
Mikhail Alekseev <malekseev@processtech.ru>
2019-12-24 10:27:25 +0300

    #1506 missed add

---
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/lang/action/AbstractUseExternalStorageFeatureDelegate.java
@@ -0,0 +1,25 @@
+package ru.runa.gpd.lang.action;
+
+import org.eclipse.jface.action.IAction;
+
+import ru.runa.gpd.lang.model.bpmn.ScriptTask;
+
+abstract class AbstractUseExternalStorageFeatureDelegate extends BaseModelActionDelegate {
+    private static final String EXTERNAL_STORAGE_HANDLER_CLASS_NAME = "ru.runa.wfe.office.storage.handler.ExternalStorageHandler";
+
+    @Override
+    public void run(IAction action) {
+        final ScriptTask scriptTask = getSelection();
+        final boolean newPropertyValue = changeProperty(scriptTask);
+
+        if (newPropertyValue) {
+            scriptTask.setDelegationClassName(EXTERNAL_STORAGE_HANDLER_CLASS_NAME);
+        } else {
+            scriptTask.setDelegationClassName(null);
+            scriptTask.setDelegationConfiguration(null);
+        }
+    }
+
+    protected abstract boolean changeProperty(ScriptTask scriptTask);
+
+}
