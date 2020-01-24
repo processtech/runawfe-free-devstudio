b0d0133f895413b992c4c7c43f2d8ca7b45e3685
Mikhail Alekseev <malekseev@processtech.ru>
2020-01-24 16:25:32 +0300

    #1505 Поправлен размер иконки на палитре

--- plugins/ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/add/AddDataStoreNodeFeature.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/add/AddDataStoreNodeFeature.java
@@ -6,10 +6,8 @@ import org.eclipse.graphiti.mm.algorithms.Rectangle;
 import org.eclipse.graphiti.mm.pictograms.ContainerShape;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.services.Graphiti;
-
 import ru.runa.gpd.editor.graphiti.GaProperty;
 import ru.runa.gpd.editor.graphiti.layout.LayoutStateNodeFeature;
-import ru.runa.gpd.lang.NodeRegistry;
 import ru.runa.gpd.lang.model.bpmn.DataStore;
 
 public class AddDataStoreNodeFeature extends AddNodeFeature {
@@ -21,7 +19,7 @@ public class AddDataStoreNodeFeature extends AddNodeFeature {
         Graphiti.getGaService().setLocationAndSize(main, context.getX(), context.getY(), context.getWidth(), context.getHeight());
         main.getProperties().add(new GaProperty(GaProperty.ID, LayoutStateNodeFeature.MAIN_RECT));
 
-        Image image = Graphiti.getGaService().createImage(main, NodeRegistry.getNodeTypeDefinition(node.getClass()).getPaletteIcon());
+        final Image image = Graphiti.getGaService().createImage(main, "datastore.png");
         image.getProperties().add(new GaProperty(GaProperty.ID, GaProperty.ICON));
         image.setTransparency(.0);
         Graphiti.getGaService().setLocationAndSize(image, -8, -8, ICON_WIDTH * 4, ICON_HEIGHT * 4);
