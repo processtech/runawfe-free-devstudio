2adcd30294c45c992a0a0cd60bf560c26cbd88b2
Mikhail Alekseev <malekseev@processtech.ru>
2020-05-08 17:58:20 +0300

    rm1394: Валидация на значение по умолчанию

--- plugins/ru.runa.gpd.office/src/ru/runa/gpd/office/store/externalstorage/InternalStorageDataModel.java
+++ plugins/ru.runa.gpd.office/src/ru/runa/gpd/office/store/externalstorage/InternalStorageDataModel.java
@@ -1,15 +1,24 @@
 package ru.runa.gpd.office.store.externalstorage;
 
+import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Iterables;
+import java.text.MessageFormat;
 import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 import org.dom4j.Document;
 import org.dom4j.Element;
+import ru.runa.gpd.lang.ValidationError;
+import ru.runa.gpd.lang.model.GraphElement;
+import ru.runa.gpd.lang.model.Variable;
 import ru.runa.gpd.office.FilesSupplierMode;
+import ru.runa.gpd.office.Messages;
 import ru.runa.gpd.office.store.DataModel;
 import ru.runa.gpd.office.store.QueryType;
 import ru.runa.gpd.office.store.StorageConstraintsModel;
+import ru.runa.gpd.office.store.externalstorage.predicate.PredicateOperationType;
 import ru.runa.gpd.util.XmlUtil;
 
 public class InternalStorageDataModel extends DataModel {
@@ -45,6 +54,49 @@ public class InternalStorageDataModel extends DataModel {
 
     public void setMode(FilesSupplierMode mode) {
         this.mode = mode;
+    }
+
+    @Override
+    public void validate(GraphElement graphElement, List<ValidationError> errors) {
+        super.validate(graphElement, errors);
+        if (!errors.isEmpty()) {
+            return;
+        }
+
+        for (StorageConstraintsModel constraintsModel : constraints) {
+            final String queryString = constraintsModel.getQueryString();
+            if (queryString == null || queryString.trim().isEmpty()) {
+                continue;
+            }
+
+            final String pattern = PredicateOperationType.codes().stream()
+                    .filter(code -> !code.equals(PredicateOperationType.AND.code) && !code.equals(PredicateOperationType.OR.code))
+                    .collect(Collectors.joining("|", "(", ")"));
+            try (Scanner expressionScanner = new Scanner(queryString)
+                    .useDelimiter("(" + PredicateOperationType.AND.code + "|" + PredicateOperationType.OR.code + ")")) {
+                while (expressionScanner.hasNext()) {
+                    try (Scanner variableScanner = new Scanner(expressionScanner.next()).useDelimiter(pattern)) {
+                        while (variableScanner.hasNext()) {
+                            final String candidate = variableScanner.next();
+                            final int i = candidate.indexOf("@");
+                            if (i == -1) {
+                                continue;
+                            }
+                            validateVariableHasDefaultValue(graphElement, errors, candidate.substring(i + 1).trim());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void validateVariableHasDefaultValue(GraphElement graphElement, List<ValidationError> errors, String variableName) {
+        final Optional<Variable> searchResult = graphElement.getProcessDefinition().getVariables(true, false).stream()
+                .filter(variable -> Objects.equal(variableName, variable.getName())).findAny();
+        if (searchResult.isPresent() && searchResult.get().getDefaultValue() == null) {
+            errors.add(ValidationError.createError(graphElement,
+                    MessageFormat.format(Messages.getString("model.validation.constraint.variable.nodefaultvalue"), variableName)));
+        }
     }
 
 }
