c6229fbce44784aa9c710826f33723114e85c10e
Mikhail Alekseev <malekseev@processtech.ru>
2020-05-13 14:13:49 +0300

    rm1394: Правка фильтрации типов данных

--- plugins/ru.runa.gpd.form.ftl/src/ru/runa/gpd/formeditor/ftl/parameter/DbUserTypeListComboParameter.java
+++ plugins/ru.runa.gpd.form.ftl/src/ru/runa/gpd/formeditor/ftl/parameter/DbUserTypeListComboParameter.java
@@ -6,11 +6,12 @@ import ru.runa.gpd.formeditor.ftl.ComboOption;
 import ru.runa.gpd.formeditor.ftl.Component;
 import ru.runa.gpd.formeditor.ftl.ComponentParameter;
 import ru.runa.gpd.formeditor.wysiwyg.FormEditor;
+import ru.runa.gpd.lang.model.VariableUserType;
 
 public class DbUserTypeListComboParameter extends ComboParameter {
     @Override
     protected List<ComboOption> getOptions(Component component, ComponentParameter parameter) {
-        return FormEditor.getCurrent().getProcessDefinition().getVariableUserTypes().stream()
+        return FormEditor.getCurrent().getProcessDefinition().getVariableUserTypes().stream().filter(VariableUserType::isStoreInExternalStorage)
                 .map(userType -> new ComboOption(userType.getName(), userType.getName())).collect(Collectors.toList());
     }
 
