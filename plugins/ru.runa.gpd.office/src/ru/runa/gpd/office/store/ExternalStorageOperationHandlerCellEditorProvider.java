19e2e710f94974fe2ba34925a24ed50a3b3933d0
Mikhail Alekseev <malekseev@processtech.ru>
2020-10-28 15:16:19 +0300

    #1891 Реализация интерфейса для Sql хэндлера

--- plugins/ru.runa.gpd.office/src/ru/runa/gpd/office/store/ExternalStorageOperationHandlerCellEditorProvider.java
+++ plugins/ru.runa.gpd.office/src/ru/runa/gpd/office/store/ExternalStorageOperationHandlerCellEditorProvider.java
@@ -1,17 +1,16 @@
 package ru.runa.gpd.office.store;
 
-import com.google.common.base.Strings;
 import java.util.Collections;
+import java.util.Optional;
 import java.util.Set;
 import ru.runa.gpd.PluginLogger;
 import ru.runa.gpd.extension.DelegableUsedDataSourcesProvider;
 import ru.runa.gpd.office.FilesSupplierMode;
 import ru.runa.gpd.office.Messages;
+import ru.runa.gpd.util.DataSourceUtils;
 
 public class ExternalStorageOperationHandlerCellEditorProvider extends BaseCommonStorageHandlerCellEditorProvider
         implements DelegableUsedDataSourcesProvider {
-    public static final String DELIMITER = ":";
-    public static final String DATASOURCE_PREFIX = "datasource";
 
     @Override
     protected FilesSupplierMode getMode() {
@@ -27,15 +26,8 @@ public class ExternalStorageOperationHandlerCellEditorProvider extends BaseCommo
     public Set<String> usedDataSourceNames(String configuration) {
         try {
             final String inputPath = fromXml(configuration).getInOutModel().inputPath;
-            if (Strings.isNullOrEmpty(inputPath)) {
-                return Collections.emptySet();
-            }
-
-            final String[] split = inputPath.split(DELIMITER);
-            if (split.length == 2 && DATASOURCE_PREFIX.equals(split[0])) {
-                return Collections.singleton(split[1]);
-            }
-            return Collections.emptySet();
+            return Optional.ofNullable(DataSourceUtils.getDataSourceNameFromParameter(inputPath)).map(Collections::singleton)
+                    .orElse(Collections.emptySet());
         } catch (Exception e) {
             PluginLogger.logError(e);
             return Collections.emptySet();
