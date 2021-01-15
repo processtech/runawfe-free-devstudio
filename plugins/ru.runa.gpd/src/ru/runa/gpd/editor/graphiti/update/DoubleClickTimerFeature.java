2b57b4f693d2b2c670e9b1b4134a0654c89c3116
betterstop <mngerasimenko@gmail.com>
2021-01-15 15:21:59 +0400

    Изменяем поведение таймера по двойном клике мышке на нем.
    Было: При двойном клике мышки на таймере открывались пареметры действия таймера.
    Исправленно: При двойном клике мышки на таймере открываються параметры задердки.
    
    Fix #2029

--- plugins/ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/update/DoubleClickTimerFeature.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/update/DoubleClickTimerFeature.java
@@ -3,11 +3,11 @@ package ru.runa.gpd.editor.graphiti.update;
 import com.google.common.base.Objects;
 import org.eclipse.graphiti.features.context.ICustomContext;
 import org.eclipse.swt.widgets.Display;
-import ru.runa.gpd.editor.graphiti.ChangeTimerActionFeature;
+import ru.runa.gpd.editor.graphiti.ChangeTimerDelayFeature;
 import ru.runa.gpd.editor.graphiti.UndoRedoUtil;
 import ru.runa.gpd.lang.model.Timer;
-import ru.runa.gpd.lang.model.TimerAction;
-import ru.runa.gpd.ui.dialog.TimerActionEditDialog;
+import ru.runa.gpd.ui.dialog.DurationEditDialog;
+import ru.runa.gpd.util.Duration;
 
 public class DoubleClickTimerFeature extends DoubleClickElementFeature {
 
@@ -18,16 +18,16 @@ public class DoubleClickTimerFeature extends DoubleClickElementFeature {
 
     @Override
     public void execute(ICustomContext context) {
-        Timer timer = (Timer) getBusinessObject(context);
-        TimerAction oldAction = timer.getAction();
-        TimerActionEditDialog dialog = new TimerActionEditDialog(timer.getProcessDefinition(), timer.getAction());
-        TimerAction result = (TimerAction) dialog.openDialog();
+    	Timer timer = (Timer) getBusinessObject(context);
+        Duration oldDelay = timer.getDelay();
+        DurationEditDialog dialog = new DurationEditDialog(timer.getProcessDefinition(), timer.getDelay());
+        Duration result = (Duration) dialog.openDialog();
         if (result != null) {
-            if (!Objects.equal(result, oldAction)) {
+        	if (!Objects.equal(result, oldDelay)) {
                 Display.getDefault().asyncExec(new Runnable() {
                     @Override
                     public void run() {
-                        UndoRedoUtil.executeFeature(new ChangeTimerActionFeature(timer, result));
+                    	UndoRedoUtil.executeFeature(new ChangeTimerDelayFeature(timer, result));
                     }
                 });
             }
