d28fbbe8edb7e3d16c1ea33e8d16a14f4b0d0a01
Allexandere <39259989+Allexandere@users.noreply.github.com>
2021-02-06 03:30:48 +0300

    task 2016

--- plugins/ru.runa.gpd/src/ru/runa/gpd/lang/model/RenameModel.java
+++
@@ -1,9 +0,0 @@
-package ru.runa.gpd.lang.model;
-
-public class RenameModel {
-
-	public RenameModel() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- plugins/ru.runa.gpd/src/ru/runa/gpd/lang/model/Renamer.java
+++
@@ -1,9 +0,0 @@
-package ru.runa.gpd.lang.model;
-
-public class Renamer {
-
-	public Renamer() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
