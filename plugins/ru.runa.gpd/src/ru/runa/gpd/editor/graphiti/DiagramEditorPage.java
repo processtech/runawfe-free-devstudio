453240270cfafbfe22cf46e07713213580a6e1bc
Stanislav Semenchuk <ssemenchuk@processtech.ru>
2022-06-01 22:30:30 +0300

    #2655 fix: update pallete

--- plugins/ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/DiagramEditorPage.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/DiagramEditorPage.java
@@ -15,6 +15,7 @@ import org.eclipse.gef.LayerConstants;
 import org.eclipse.gef.commands.CommandStack;
 import org.eclipse.gef.editparts.LayerManager;
 import org.eclipse.gef.editparts.ScalableFreeformRootEditPart;
+import org.eclipse.gef.palette.PaletteRoot;
 import org.eclipse.graphiti.features.IAddFeature;
 import org.eclipse.graphiti.features.IFeatureProvider;
 import org.eclipse.graphiti.features.IUpdateFeature;
@@ -400,5 +401,9 @@ public class DiagramEditorPage extends DiagramEditor implements PropertyChangeLi
             }
         });
         getDiagramBehavior().refresh();
+    }
+
+    public PaletteRoot paletteRoot() {
+    	return super.getPaletteRoot();
     }
 }
