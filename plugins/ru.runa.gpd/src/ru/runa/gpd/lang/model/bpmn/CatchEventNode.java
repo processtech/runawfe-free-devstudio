18154a63264620e6fbe28f03962bf43a51856e0a
Ivan <zin1995@yandex.ru>
2022-08-30 08:30:18 +0300

    Bug #2640

--- plugins/ru.runa.gpd/src/ru/runa/gpd/lang/model/bpmn/CatchEventNode.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/lang/model/bpmn/CatchEventNode.java
@@ -2,9 +2,13 @@ package ru.runa.gpd.lang.model.bpmn;
 
 import java.util.List;
 
+import org.eclipse.core.resources.IFile;
+
 import ru.runa.gpd.lang.ValidationError;
 import ru.runa.gpd.lang.model.IReceiveMessageNode;
+import ru.runa.gpd.lang.model.Node;
 import ru.runa.gpd.lang.model.Timer;
+import ru.runa.gpd.lang.model.Transition;
 
 public class CatchEventNode extends AbstractEventNode implements IReceiveMessageNode, IBoundaryEvent, IBoundaryEventContainer {
 
@@ -20,4 +24,21 @@ public class CatchEventNode extends AbstractEventNode implements IReceiveMessage
         }
         super.validateOnEmptyRules(errors);
     }
+    
+	@Override
+	protected boolean allowArrivingTransition(Node source, List<Transition> transitions) {
+		if (getParent() instanceof IBoundaryEvent) {
+			// boundary event
+			return false;
+		}
+		return super.allowArrivingTransition(source, transitions);
+	}
+
+	@Override
+	public void validate(List<ValidationError> errors, IFile definitionFile) {
+		super.validate(errors, definitionFile);
+		if (getParent() instanceof IBoundaryEvent && getArrivingTransitions().size() > 0) {
+			errors.add(ValidationError.createLocalizedError(this, "unresolved Arriving Transition"));
+		}
+	}
 }
