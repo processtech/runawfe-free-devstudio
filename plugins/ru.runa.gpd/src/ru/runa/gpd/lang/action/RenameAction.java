3cf276da2fcdc6c1beea2536763d41afe50d6e86
Ivan Zikin <inzikin@processtech.ru>
2022-12-21 13:18:06 +0300

    rm2884 rename action added to menu for all named elements

--- plugins/ru.runa.gpd/src/ru/runa/gpd/lang/action/RenameAction.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/lang/action/RenameAction.java
@@ -3,17 +3,16 @@ package ru.runa.gpd.lang.action;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.swt.widgets.Display;
-
 import ru.runa.gpd.Localization;
 import ru.runa.gpd.PluginLogger;
-import ru.runa.gpd.lang.model.Node;
+import ru.runa.gpd.lang.model.NamedGraphElement;
 
 public class RenameAction extends BaseModelActionDelegate {
 
     @Override
     public void run(IAction action) {
-        Node node = getSelection();
-        String currentName = node.getName();
+        NamedGraphElement element = getSelection();
+        String currentName = element.getName();
         InputDialog renameNodeDialog = new InputDialog(Display.getDefault().getActiveShell(), null, Localization.getString("InputValue"), currentName,
                 (String value) -> {
                     value = value.trim();
@@ -26,7 +25,7 @@ public class RenameAction extends BaseModelActionDelegate {
                 });
         try {
             if (renameNodeDialog.open() == InputDialog.OK) {
-                node.setName(renameNodeDialog.getValue().trim());
+                element.setName(renameNodeDialog.getValue().trim());
             }
         } catch (Exception e) {
             PluginLogger.logError(e);
