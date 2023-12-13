09b2b4c86e209f0c5bec829c2cb091c10cb0fdd0
Dofs Serge <dofs197@gmail.com>
2023-12-13 11:32:55 +0300

    rm3262 IndependenceSolver improved

--- plugins/ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/IndependenceSolver.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/editor/graphiti/IndependenceSolver.java
@@ -2,20 +2,19 @@ package ru.runa.gpd.editor.graphiti;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import org.eclipse.graphiti.features.impl.IIndependenceSolver;
 
 public class IndependenceSolver implements IIndependenceSolver {
-    private Map<String, Object> objectMap = new HashMap<String, Object>();
+    // rm3262#note-11 static for memory optimization
+    // despite of one instance per DiagramFeatureProvider, all instances are called with all opened diagram entities
+    private static Map<String, Object> objectMap = new HashMap<>();
 
     @Override
     public String getKeyForBusinessObject(Object bo) {
         String result = null;
         if (bo != null) {
-            result = String.valueOf(bo.hashCode());
-            if (!objectMap.containsKey(result)) {
-                objectMap.put(result, bo);
-            }
+            result = String.valueOf(System.identityHashCode(bo));
+            objectMap.put(result, bo);
         }
         return result;
     }
