9a91b900e0e2b2ef24802390bc60731795f8b9b3
Pletenev Andrey <pletenev.andrey@yandex.ru>
2024-02-13 13:50:39 +0300

    rm3388 added refresh of property page when element's property changed

--- plugins/ru.runa.gpd/src/ru/runa/gpd/ui/view/OrderedPropertySheetPage.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/ui/view/OrderedPropertySheetPage.java
@@ -1,11 +1,20 @@
 package ru.runa.gpd.ui.view;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import org.eclipse.graphiti.ui.internal.parts.ContainerShapeEditPart;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.views.properties.IPropertySheetEntry;
 import org.eclipse.ui.views.properties.PropertySheetPage;
 import org.eclipse.ui.views.properties.PropertySheetSorter;
+import ru.runa.gpd.lang.model.GraphElement;
 
-public class OrderedPropertySheetPage extends PropertySheetPage {
+public class OrderedPropertySheetPage extends PropertySheetPage implements PropertyChangeListener {
+
+    private GraphElement propertySource;
 
     public OrderedPropertySheetPage() {
         super();
@@ -14,8 +23,7 @@ public class OrderedPropertySheetPage extends PropertySheetPage {
             public int compare(IPropertySheetEntry entryA, IPropertySheetEntry entryB) {
                 return 1; // Do not changes the logical order of properties. Prevents default alphabetical sorting.
             }
-        }
-        );
+        });
     }
 
     @Override
@@ -23,5 +31,29 @@ public class OrderedPropertySheetPage extends PropertySheetPage {
         super.createControl(parent);
         // 3274#note-8
         getControl().setMenu(null);
+    }
+
+    @Override
+    public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+        if (propertySource != null) {
+            propertySource.removePropertyChangeListener(this);
+        }
+        super.selectionChanged(part, selection);
+        if (selection instanceof IStructuredSelection && !selection.isEmpty()) {
+            Object element = ((IStructuredSelection) selection).getFirstElement();
+            if (element instanceof ContainerShapeEditPart) {
+                ContainerShapeEditPart container = (ContainerShapeEditPart) element;
+                Object bo = container.getFeatureProvider().getBusinessObjectForPictogramElement(container.getPictogramElement());
+                if (bo instanceof GraphElement) {
+                    propertySource = (GraphElement) bo;
+                    propertySource.addPropertyChangeListener(this);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        refresh();
     }
 }
