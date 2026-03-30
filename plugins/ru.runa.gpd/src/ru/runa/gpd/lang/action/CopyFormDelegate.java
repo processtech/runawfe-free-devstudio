ddb8755885511a53607bfab55ddf929d7efd9295
HackDwenture <kiriklass228reserv@mail.ru>
2026-03-30 22:35:02 +0300

    fix: remove old form action disabling for start node of embedded subprocess

--- plugins/ru.runa.gpd/src/ru/runa/gpd/lang/action/CopyFormDelegate.java
+++ plugins/ru.runa.gpd/src/ru/runa/gpd/lang/action/CopyFormDelegate.java
@@ -3,44 +3,18 @@ package ru.runa.gpd.lang.action;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.window.Window;
-import ru.runa.gpd.Localization;
 import ru.runa.gpd.PluginLogger;
 import ru.runa.gpd.lang.model.FormNode;
-import ru.runa.gpd.lang.model.StartState;
-import ru.runa.gpd.lang.model.SubprocessDefinition;
-import ru.runa.gpd.ui.custom.Dialogs;
 import ru.runa.gpd.ui.dialog.CopyFormDialog;
 import ru.runa.gpd.util.IOUtils;
 
 public class CopyFormDelegate extends FormDelegate {
 
     @Override
-    public void selectionChanged(IAction action, ISelection selection) {
-        super.selectionChanged(action, selection);
-        if (selection instanceof IStructuredSelection) {
-            Object element = ((IStructuredSelection) selection).getFirstElement();
-            if (element instanceof StartState) {
-                StartState startState = (StartState) element;
-                if (startState.getProcessDefinition() instanceof SubprocessDefinition) {
-                    action.setEnabled(false);
-                    return;
-                }
-            }
-        }
-        action.setEnabled(true);
-    }
-
-    @Override
     public void run(IAction action) {
         try {
             FormNode formNode = getSelection();
-            if (formNode instanceof StartState && formNode.getProcessDefinition() instanceof SubprocessDefinition) {
-                Dialogs.error(Localization.getString("error.startState.formNotAllowedInEmbeddedSubprocess"));
-                return;
-            }
             CopyFormDialog copyFormDialog = new CopyFormDialog(formNode);
             if (copyFormDialog.open() != Window.OK) {
                 return;
